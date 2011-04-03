9ac9363587a051683224b19a5aad399a3d7a3908
Joshua Simmons <simmons.44@gmail.com>
2011-04-04 08:33:10 +1000

    Adding some basic testing.
    For now this is just a parser sanity check, but later we should
    add more comprehensive testing.

---
+++ tests/request.lua
@@ -0,0 +1,24 @@
+local request = require 'mongrel2.request'
+
+local payloads = {}
+
+for line in io.lines('request_payloads.txt') do
+    table.insert(payloads, line)
+end
+
+context('m2-lua', function()
+    
+    context('request', function()
+        context('parser sanity', function()
+            for _, msg in pairs(payloads) do
+                 test(msg, function()
+                     local req, err = request.parse(msg)
+                     assert_nil(err)
+                     assert_not_nil(req)
+                 end)
+            end
+        end)
+
+    end)
+
+end)

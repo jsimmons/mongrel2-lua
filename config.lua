45922e19ff784ca0061543d90d7968e0990f462c
Joshua Simmons <simmons.44@gmail.com>
2011-05-12 07:30:03 +1000

    Undo API breaking change on master branch

--- config.lua
+++ config.lua
@@ -291,12 +291,7 @@ function read(db_file)
         table.insert(servers, server)
     end
 
-    local settings = {}
-    for _, setting in pairs(db_select(db, 'setting')) do
-        settings[setting.key] = setting.value
-    end
-
     db:close()
 
-    return {servers = servers; settings = settings}
+    return servers
 end
